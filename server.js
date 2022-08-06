a0b1d7d0479b95d9d7ab5f19b6628d5057197621
Zubayer Hossain <zubayer94@gmail.com>
2022-08-06 20:35:56 +0600

    basic boilerplate

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const config = require('config')
+const app = express()
+const {sequelize} = require('./models/index')
+
+// app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+
+/**
+ * Create a anonymous function to establish the database connection.
+ * After the connection is established, start the server.
+ */
+ const initApp = async () => {
+    console.log(config.get('db.password'));
+    console.log("Testing the database connection..");
+    /**
+     * Test the connection.
+     * You can use the .authenticate() function to test if the connection works.
+     */
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+
+        /**
+         * Start the web server on the specified port.
+         */
+         app.listen(port, () => {
+            console.log(`listening at ${port}`);
+        })
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+/**
+ * Initialize the application.
+ */
+initApp();
